56328f6a08720a7ae9cb727ec9195fef0189e045
shreejana012 <shrijana460@gmail.com>
2018-12-19 11:10:53 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovie] = useState([]);
+
+  const searchMovies = () => {
+    const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=9973fd8b&s=${searchQuery}`
+    axios.get(API_URL)
+    .then(function (response) {
+      // handle success
+      setMovie(response.data.Search)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    return movies;
+  };
+
+  const handleMovieInput = e => {
+    setSearchQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `${searchQuery}`;
+  });
+
+  return (
+    <div>
+      <h3> Search Movies </h3>
+      <input
+        type="text"
+        onChange={handleMovieInput}
+        value={searchQuery}
+        placeholder="Search something"
+      />
+      <button onClick={searchMovies}> Search </button>
+      {movies.map(function(m, i){
+        return <li key={i}>
+            {m.Title}<br />
+            <img src = {m.Poster} /><br />
+            {m.Type}<br />
+            {m.Year}<br />
+            {m.imdbID}
+          </li>
+        })
+      }
+    </div>
+  );
+};
 
 export default App;
